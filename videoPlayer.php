170e79473f0c596c23bb433dd7de2e4722426739
randomlysa <su@randomlysa.com>
2017-02-18 01:29:34 -0600

    update to match player in admin section

--- videoPlayer.php
+++ videoPlayer.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta charset="utf-8">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
     </head>
   <body>
 
@@ -13,22 +14,28 @@
             <div class="col-md-3"></div>
 
             <div class="col-md-6">
-                <div class="videoJSembed" data-bind="foreach: currentVideo">
-                    <h2 data-bind="text: title()"></h2>
+                <!-- ko foreach: currentVideo -->
+                <h2 data-bind="text: title()"></h2><br>
+                <div class="videoJSembed">
                     <iframe data-bind="attr: {'src': 'videoJSframe.php?source=' + filename() }" id="videoPlayerFrame" allowfullscreen=" allowfullscreen" height="370" style="width: 100%; text-align: center; border: none; padding: none;"></iframe>
                 </div>
+                <!-- /ko -->
+                <div>
+                    <table id="listOfRecordingsTable" width="100%">
+                        <thead>
+                            <tr>
+                                <th>Date/Time</th>
+                                <th>Title</th></tr>
+                        </thead>
+                        <tbody data-bind="foreach: listOfRecordings">
+                            <tr data-bind="click: $parent.setLoadedInPlayerVideo, css: {recording: status, loadedInPlayer: loadedInPlayer}">
+                                <td data-bind="text: datetime"></td>
+                                <td data-bind="text: title"></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
 
-                <table id="listOfRecordings" width="100%">
-                    <thead>
-                        <tr><th>Date/Time</th><th>Title</th></tr>
-                    </thead>
-                    <tbody data-bind="foreach: listOfRecordings">
-                        <tr data-bind="click: $parent.setVideoPlayerFile">
-                            <td data-bind="text: datetime"></td>
-                            <td data-bind="text: title"></td>
-                        </tr>
-                    </tbody>
-                </table>
             <div class="col-md-3"></div>
         </div>
     </div>
