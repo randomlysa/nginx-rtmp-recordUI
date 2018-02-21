525e07f13e5c4e1898101bbf195ff5e2edad50b4
randomlysa <su@randomlysa.com>
2018-02-20 22:44:55 -0600

    update videojs to newest version

--- videoJSframe.php
+++ videoJSframe.php
@@ -1,4 +1,5 @@
 <?php
+// This is the file that actually plays the video.
 require_once ('config.php');
 $source = $_GET["source"];
 ?>
@@ -9,9 +10,8 @@ $source = $_GET["source"];
     <title>videoJS iframe</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="utf-8">
-    <link href="http://vjs.zencdn.net/5.16.0/video-js.css" rel="stylesheet">
-    <!-- If you'd like to support IE8 -->
-    <script src="http://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"></script>
+    <link href="http://vjs.zencdn.net/6.6.3/video-js.css" rel="stylesheet">
+
   </head>
   <body>
     <video id="my-video" class="video-js vjs-default-skin vjs-16-9 vjs-big-play-centered" controls preload="auto" data-setup="{}">
@@ -21,6 +21,7 @@ $source = $_GET["source"];
             <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
         </p>
     </video>
-    <script src="http://vjs.zencdn.net/5.16.0/video.js"></script>
+    <script src="http://vjs.zencdn.net/6.6.3/video.js"></script>
+
   </body>
 </html>
